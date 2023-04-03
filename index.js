a74f652efd124ecfddcf06fbc05668a64161bfda
Daniel Lee <dlee923@gmail.com>
2023-04-03 16:36:24 -0400

    modified generic

--- index.js
+++ index.js
@@ -24,14 +24,12 @@ app.get("/api/hello", function (req, res) {
   res.json({greeting: 'hello API'});
 });
 
-app.get("/api/:dateparam", function (req, res, next) {
-  // let newDate = newDate(req.params.dateparam);
-  next();  
-}, function(req, res) {
+app.get("/api/:dateparam", function (req, res) {
+  let newDate = req.params.dateparam;
   res.json({
     query: req,
     params: req.params,
-    // dateparam: newDate
+    dateparam: newDate
   })
 });
 

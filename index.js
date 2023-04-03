365b4dd59b8bb2d4ee5699f89de939fa279975fd
Daniel Lee <dlee923@gmail.com>
2023-04-03 15:49:00 -0400

    modified options

--- index.js
+++ index.js
@@ -34,7 +34,7 @@ app.get('/api/2015-12-25', function(req, res) {
 app.get('/api/1451001600000', function(req, res) {
   let newDate = new Date(1451001600000);
   let options = {dateStyle: 'medium', timestyle: 'long', timeZone: 'GMT'};
-  let utc = new Intl.DateTimeFormat('en-US', options).format(newDate);
+  let utc = new Intl.DateTimeFormat('en-GB', options).format(newDate);
 
   res.json({
     unix: 1451001600000,
